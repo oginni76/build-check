54285363a95e505dd980375cb6c2f82b0ed6018d
oginni76 <oginniemmanuel040@gmail.com>
2026-01-30 09:14:53 +0100

    removed intentional break commit syntax error test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const x: number = "I am a string";.
     return 'Hello World!';
   }
 }
