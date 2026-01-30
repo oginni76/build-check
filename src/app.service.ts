614f5132e5aac9fc3e87e78ac8b0e78da312da62
oginni76 <oginniemmanuel040@gmail.com>
2026-01-30 09:07:27 +0100

    intentioal break

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const x: number = "I am a string";.
     return 'Hello World!';
   }
 }
