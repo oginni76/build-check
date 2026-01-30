57685c5f66e483e21bbd34ae620db303e4a49589
oginni76 <oginniemmanuel040@gmail.com>
2026-01-30 09:46:07 +0100

    still on pr test

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getHi(): string {
     return 'Hello World. Using this pr branch to test!';
   }
 }
